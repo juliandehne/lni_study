669107a34aecabfbc7a53524e7e3e05f9aa89459
Lukka <lukka.mayer@gmail.com>
2026-07-27 13:51:51 +0200

    Delete Local_LLM directory

--- Local_LLM/Closest_superior_power_of_2.py
+++
@@ -1,9 +0,0 @@
-
-def power_of_2(n, pow=2):
-    if pow > n:
-        return pow
-    return power_of_2(n, 2 * pow)
-
-
-a = int(input("Please enter a number: "))
-print(power_of_2(a))
--- Local_LLM/test_1.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from pypdf import PdfReader
-
-
-print(len(PdfReader("Papers/12.pdf").pages))
-
